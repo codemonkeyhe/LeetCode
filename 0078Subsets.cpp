fb9e3e25b64faf1441e6b6a7106e38df6b207d90
codemonkeyhe <328679181@qq.com>
2020-08-09 11:21:25 +0800

    redo 78-90

--- 0078Subsets.cpp
+++ 0078Subsets.cpp
@@ -4,7 +4,7 @@
  * @author MonkeyHe
  * @version  1.0
  * @date 2020-08-06
- * @tag
+ * @tag backtracking
  */
 
 /*
@@ -37,7 +37,13 @@ class Solution {
    public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
-        m1(nums, res);
+        m3(nums, res);
+
+        //m1(nums, res);
+
+        // vector<int> path;
+        // m2(nums, 0, path, res);
+
         return res;
     }
 
@@ -57,6 +63,37 @@ class Solution {
             }
         }
     }
+
+//Runtime: 4 ms, faster than 73.12% of C++ online submissions for Subsets.
+//Memory Usage: 7.1 MB, less than 73.96% of C++ online submissions for Subsets.
+    void m2(vector<int>& nums, int begin, vector<int>& path, vector<vector<int>>& res) {
+        res.push_back(path);
+        for (int i = begin; i < nums.size(); ++i) {
+            path.push_back(nums[i]);
+            m2(nums, i+1, path, res);
+            path.pop_back();
+        }
+    }
+
+//Runtime: 4 ms, faster than 72.71% of C++ online submissions for Subsets.
+//Memory Usage: 7.1 MB, less than 85.10% of C++ online submissions for Subsets.
+    void m3(vector<int>& nums,  vector<vector<int>>& res) {
+        int len = nums.size();
+        int total = 1 << len;  // 2^len
+        int cur = 0;
+        for (int i = 0; i < total; ++i) {
+            cur = i;
+            vector<int> subset;
+            for (int j = 0; j < len; ++j) {
+                if (cur&0x01) {
+                    subset.push_back(nums[j]);
+                }
+                cur >>= 1;
+            }
+            res.push_back(subset);
+        }
+    }
+
 
 };
 
@@ -81,12 +118,27 @@ int main() {
 
 /*
 Tips
-M1 构造法  来自CCI
+M1 构造法-循环版  来自CCI
 S(n)为n个元素的组合的集合
 把S(n-1)的所有组合全部复制一遍，在每个组合里加入a[n]，即可得到S(n)
 即 S(n) = S(n-1) + { ComIn_S(n-1), a[n]}
 
 
-M2
+M2 构造法-回溯版
+
+M3 二进制转换法
+3个元素的子集数量为2^3=8
+N个元素的子集数量为2^N
+因为构造子集时，每个元素可以选择加入或者不加入，总共有N个元素，所以是2^N
+
+把加入当做二进制1，不加入当做二进制0
+3个元素的子集对应3个bit位,如下所示
+000 空集
+001 a
+010 b
+100 c
+101 {a,c}
+111 {a,b,c}
+生成二进制bit序列，再把序列转为子集即可
 
 */
--- 0090SubsetsII.cpp
+++ 0090SubsetsII.cpp
@@ -4,6 +4,7 @@
  * @author MonkeyHe
  * @version  1.0
  * @date 2020-08-06
+ * @tag backtracking; DFS;
  */
 
 /*
@@ -35,12 +36,32 @@ class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> res;
-        m1(nums, res);
+
+        vector<int> path;
+        sort(nums.begin(), nums.end());
+        m2(nums, 0, path, res);
+
+      //  m1(nums, res);
         return res;
     }
 
+    //   skill 同层去重
+    // Runtime: 0 ms, faster than 100.00% of C++ online submissions for Subsets II.
+    // Memory Usage: 7.6 MB, less than 91.35% of C++ online submissions for Subsets II.
+    void m2(vector<int>& nums, int begin, vector<int>& path, vector<vector<int>>& res) {
+        res.push_back(path);
+        for (int i = begin; i < nums.size(); ++i) {
+            if (i>begin && nums[i] == nums[i-1])
+                continue;
+            path.push_back(nums[i]);
+            m2(nums, i+1, path, res);
+            path.pop_back();
+        }
+    }
+
+
 //Runtime: 12 ms, faster than 30.04% of C++ online submissions for Subsets II.
-//Memory Usage: 7.9 MB, less than 59.09% of C++ online submissions for Subsets II.
+//Memory Usage: 7.9 MB, less than 59.09% of C++ online 3submissions for Subsets II.
     void m1(vector<int>& nums,  vector<vector<int>>& res) {
         if (nums.size() == 0)
             return;
@@ -78,8 +99,8 @@ int** subsetsWithDup(int* nums, int numsSize, int* returnSize, int** returnColum
 
 
 int main() {
-    const int len = 4;
-    int nums[len] = {1, 2, 2, 2};
+    const int len = 3;
+    int nums[len] = {1, 2, 2};
     vector<int> nu(nums, nums + len);
     Solution s;
     auto res = s.subsetsWithDup(nu);
@@ -94,6 +115,11 @@ Tips
 M1 构造法 同0078
 只是要把元素先排序，相同的元素添加时，要区分界限，添加元素时不再需要拷贝整个全集了，而是拷贝上一次的增量
 
-M2
+M2 构造法-递归
+skill 同层去重 同0040
+
+M3 二进制
+不可行 或者说 可行但是性能太低
+去重不好做
 
 */
