39a65fce68a9d76e5874d0248fa0591ee0346362
monkey <codemonkeyhe@gmail.com>
2025-08-28 22:19:31 +0800

    chore:20250828 linkedList  725 done

---
+++ 0725SplitLinkedListInParts.cpp
@@ -0,0 +1,169 @@
+/**
+ * @file
+ * @brief
+ * @author MonkeyHe
+ * @version  1.0
+ * @date 2025-08-28
+ * @tag  linkedList
+ * @similar  61, 328, 725
+ */
+
+/*
+
+*/
+
+
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "common.h"
+#include <iostream>
+
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=725 lang=cpp
+ *
+ * [725] 分隔链表
+ *
+ * https://leetcode.cn/problems/split-linked-list-in-parts/description/
+ *
+ * algorithms
+ * Medium (60.69%)
+ * Likes:    330
+ * Dislikes: 0
+ * Total Accepted:    69.9K
+ * Total Submissions: 113.8K
+ * Testcase Example:  '[1,2,3]\n5'
+ *
+ * 给你一个头结点为 head 的单链表和一个整数 k ，请你设计一个算法将链表分隔为 k 个连续的部分。
+ * 每部分的长度应该尽可能的相等：任意两部分的长度差距不能超过 1 。这可能会导致有些部分为 null 。
+ * 这 k 个部分应该按照在链表中出现的顺序排列，并且排在前面的部分的长度应该大于或等于排在后面的长度。
+ * 返回一个由上述 k 部分组成的数组。
+ *
+ *
+ * 示例 1：
+ * 输入：head = [1,2,3], k = 5
+ * 输出：[[1],[2],[3],[],[]]
+ * 解释：
+ * 第一个元素 output[0] 为 output[0].val = 1 ，output[0].next = null 。
+ * 最后一个元素 output[4] 为 null ，但它作为 ListNode 的字符串表示是 [] 。
+ *
+ *
+ * 示例 2：
+ * 输入：head = [1,2,3,4,5,6,7,8,9,10], k = 3
+ * 输出：[[1,2,3,4],[5,6,7],[8,9,10]]
+ * 解释：
+ * 输入被分成了几个连续的部分，并且每部分的长度相差不超过 1 。前面部分的长度大于等于后面部分的长度。
+ *
+ *
+ *
+ *
+ * 提示：
+ * 链表中节点的数目在范围 [0, 1000]
+ * 0 <= Node.val <= 1000
+ * 1 <= k <= 50
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // g=11/3 = 3  more=11%3 = 2   3+1 3+1 3
+    // 1 2 3 4    5 6 7 8   9 10 11
+    // g=3/5=0 more=3%5=3;
+    vector<ListNode*> splitListToPartsV1(ListNode* head, int k) {
+        ListNode* p = head;
+        int cnt = 0;
+        while (p != NULL) {
+            cnt++;
+            p = p->next;
+        }
+        vector<ListNode*> res;
+        int g = cnt / k;
+        int more = cnt % k;
+        p = head;
+        while (p != NULL) {
+            res.push_back(p);
+            int batch = g;
+            if (more > 0) {
+                batch++;
+                more--;
+            }
+            while (p != NULL) {
+                if (batch == 1) {
+                    ListNode* tail = p;
+                    p = p->next;
+                    tail->next = NULL;
+                    break;
+                }
+                p = p->next;
+                batch--;
+            }
+        }
+        while (res.size() < k) {
+            res.push_back(NULL);
+        }
+        return res;
+    }
+
+    // v2
+    vector<ListNode* > splitListToParts(ListNode *head, int k) {
+        ListNode* p = head;
+        int cnt = 0;
+        while (p != NULL) {
+            cnt++;
+            p = p->next;
+        }
+        vector<ListNode*> res(k, NULL);
+        int g = cnt / k;
+        int more = cnt % k;
+        p = head;
+        int j = 0;
+        while (p != NULL) {
+            res[j++] = p;
+            int batch = g;
+            if (more > 0) {
+                batch++;
+                more--;
+            }
+            while (p != NULL) {
+                if (batch == 1) {
+                    ListNode* tail = p;
+                    p = p->next;
+                    tail->next = NULL;
+                    break;
+                }
+                p = p->next;
+                batch--;
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
+
+int main() {
+    Solution s;
+}
+
+
+/*
+Tips
+M1
+
+M2
+
+*/
