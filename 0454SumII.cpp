3266a3aad5f96d7cdc71beb2686d94081e7e3ece
monkey <codemonkeyhe@gmail.com>
2025-11-06 20:41:34 +0800

    feat: 20251106 454 hashtable done

---
+++ 0454SumII.cpp
@@ -0,0 +1,141 @@
+/**
+ * @file
+ * @brief
+ * @author MonkeyHe
+ * @version  1.0
+ * @date 2025-11-06
+ * @tag     hashTable
+ * @similar   18, 454
+ */
+
+/*
+
+*/
+
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "common.h"
+#include <iostream>
+
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=454 lang=cpp
+ *
+ * [454] 四数相加 II
+ *
+ * https://leetcode.cn/problems/4sum-ii/description/
+ *
+ * algorithms
+ * Medium (64.49%)
+ * Likes:    1010
+ * Dislikes: 0
+ * Total Accepted:    276.7K
+ * Total Submissions: 428.9K
+ * Testcase Example:  '[1,2]\n[-2,-1]\n[-1,2]\n[0,2]'
+ *
+ * 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l)
+ * 能满足：
+ * 0 <= i, j, k, l < n
+ * nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+ *
+ *
+ *
+ *
+ * 示例 1：
+ * 输入：nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+ * 输出：2
+ * 解释：
+ * 两个元组如下：
+ * 1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) +
+ * (-1) + 2 = 0
+ * 2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) +
+ * (-1) + 0 = 0
+ *
+ *
+ * 示例 2：
+ * 输入：nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ * n == nums1.length
+ * n == nums2.length
+ * n == nums3.length
+ * n == nums4.length
+ * 1 <= n <= 200
+ * -2^28 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 2^28
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int, int> valCnt;
+        for (int i = 0; i < nums1.size(); i++) {
+            for (int j = 0; j < nums2.size(); j++) {
+                int sum = nums1[i] + nums2[j];
+                valCnt[sum]++; // means <i, j> = sum
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < nums3.size(); i++) {
+            for (int j = 0; j < nums4.size(); j++) {
+                int sum = nums3[i] + nums4[j];
+                if (valCnt.count(-sum)!=0) {
+                    cnt += valCnt[-sum];
+                }
+            }
+        }
+        return cnt;
+    }
+
+};
+// @lc code=end
+
+
+/*
+golang
+
+func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
+	valCnt := make(map[int]int)
+	for _, n1 := range nums1 {
+		for _, n2 := range nums2 {
+			sum := n1 + n2
+			valCnt[sum]++
+		}
+	}
+	cnt := 0
+	for _, n3 := range nums3 {
+		for _, n4 := range nums4 {
+			sum := n3 + n4
+			if v, ok := valCnt[-sum]; ok {
+				cnt += v
+			}
+		}
+	}
+	return cnt
+}
+
+
+*/
+
+
+int main() {
+    Solution s;
+}
+
+
+/*
+Tips
+M1
+
+M2
+
+*/
