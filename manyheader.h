f5fbd3354aae09a2359d299cb6cafd5edf7e30a6
monkey <codemonkeyhe@gmail.com>
2025-11-07 19:31:04 +0800

    chore: add headers

---
+++ manyheader.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <unordered_map>
+#include <unordered_set>
+// NO SUCH THING  #include <priority_queue>
+
+using namespace std;
