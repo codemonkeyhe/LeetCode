d9df5b360d816b275a1fa66c8874c7ebf2ddc1cd
monkey <codemonkeyhe@gmail.com>
2025-12-02 13:28:03 +0800

    feat: 20251202 176 2nd HighSalary SQL done

---
+++ 31mySQL/176SecondHighestSalary.sql
@@ -0,0 +1,82 @@
+/**
+ * @file
+ * @brief
+ * @author MonkeyHe
+ * @version  1.0
+ * @date 2025-12-02
+ * @tag   sql; max
+ * @similar
+ */
+
+/*
+Employee 表：
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id 是这个表的主键。
+表的每一行包含员工的工资信息。
+
+
+查询并返回 Employee 表中第二高的 不同 薪水 。如果不存在第二高的薪水，查询应该返回 null(Pandas 则返回 None) 。
+
+查询结果如下例所示。
+
+
+
+示例 1：
+
+输入：
+Employee 表：
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+输出：
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+示例 2：
+
+输入：
+Employee 表：
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+输出：
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+*/
+
+
+--M1
+with t as (
+select distinct salary
+from employee
+order by salary desc
+limit 1,1 )
+select IFNULL(
+    (select salary from t),
+    NULL) as SecondHighestSalary
+
+
+--M2
+with t as (
+select max(salary) as maxVal
+from employee)
+select max(salary) as SecondHighestSalary
+from employee, t
+where salary < t.maxVal
+
